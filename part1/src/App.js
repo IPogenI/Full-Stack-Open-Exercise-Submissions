44b99136d05edc3d999d2d89c3b904859bcd9d3f
Pogen <76914800+IPogenI@users.noreply.github.com>
2022-12-12 16:19:15 +0600

    component helper functions

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,24 @@
-const Header = (props) => {
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
-      <h1>{props.course.name}</h1>
+      <p>Hello {props.name}, you must be {props.age} years old.</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
-  )
-}
-
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </div>
-  )
-}
-
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-  <div>
-    <p>
-      Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-    </p>
-  </div>
   )
 }
 
 
 const App = () => {
-  const course = {
-      name: 'Half Stack application development',
-      parts : [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
+  const name = "Prottoy"
+  const age = 21
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
