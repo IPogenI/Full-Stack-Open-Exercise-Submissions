197d155e4d5428d3be5078e7758e6f629bf30572
Pogen <76914800+IPogenI@users.noreply.github.com>
2022-12-12 16:28:43 +0600

    destructuring

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,11 +1,10 @@
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+const Hello = ({ name, age }) => {
+  
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you must be {props.age} years old.</p>
+      <p>Hello {name}, you must be {age} years old.</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
