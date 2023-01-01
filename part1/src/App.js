a27fba395a65f72e12d449092f2c764739c54513
Pogen <76914800+IPogenI@users.noreply.github.com>
2023-01-01 13:40:48 +0600

    creating smaller react components

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,19 +4,30 @@ const App = () => {
   const [counter, setCounter] = useState(0)
   
   const increaseByOne = () => setCounter(counter + 1)
-
+  const setToZero = () => setCounter(0)
   const decreaseByOne = () => setCounter(counter - 1)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={decreaseByOne}> 
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text="plus"/>
+      <Button onClick={decreaseByOne} text="minus"/>
+      <Button onClick={setToZero} text="zero"/>
     </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
